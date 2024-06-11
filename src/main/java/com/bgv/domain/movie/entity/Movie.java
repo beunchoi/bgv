bc3a9f69bcf799b6d7dd4f1d6751791ceb44b18d
beunchoi <byungeun0228@gmail.com>
2024-06-12 00:28:45 +0900

    feat : movie 엔티티 생성

--- src/main/java/com/bgv/domain/movie/entity/Movie.java
+++ src/main/java/com/bgv/domain/movie/entity/Movie.java
@@ -1,5 +1,24 @@
 package com.bgv.domain.movie.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Movie {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String movieName;
+
+  @Column(nullable = false)
+  private String description;
 
 }
